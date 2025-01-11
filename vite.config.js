45fb1759aeab4275e6bcafc9b71cbb89b2732687
Dariya Hussein Kondapalli <dariya.k007@gmail.com>
2025-01-11 16:40:10 +0530

    ggg

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/', // Default for Vercel (no subdirectory)
+  build: {
+    outDir: 'dist', // Explicitly specify the output directory
+  },
 })
